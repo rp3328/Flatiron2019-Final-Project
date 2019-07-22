0bb96074309c6a784d374eae04a6b73cc510b3a2
github username <github email address>
2019-07-22 14:07:41 -0400

    done

--- frontend-finbot/src/index.js
+++ frontend-finbot/src/index.js
@@ -1,52 +1,30 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
-let divTag = document.querySelector('main')
+//Partial function
 
+//login form
+function login(){
+  
+`<form action="action_page.php">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
-document.addEventListener('DOMContentLoaded', function(){
-    fetch(TRAINERS_URL)
-    .then(res => res.json())
-    .then(trainers => {
-        trainers.forEach((trainer) => {
-            divTag.innerHTML += `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
-            <button class="add" data-trainer-id=${trainer.id}>Add Pokemon</button>
-            <ul id="trainer_${trainer.id}">
-                
-            </ul>
-          </div>`
-        let ulTag = document.querySelector(`#trainer_${trainer.id}`)
-        trainer.pokemons.forEach((pokemon) => {
-            ulTag.innerHTML += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
-        })
-        })
-    })
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-    document.addEventListener('click', function(e) {
-        if(e.target.tagName === 'BUTTON' && e.target.className === 'release'){
-            fetch(`${BASE_URL}/pokemons/${event.target.dataset.pokemonId}`, {
-                method: "DELETE"
-            })
-            .then(data => {
-                e.target.parentElement.remove()
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-            })
-        }else if(e.target.tagName === 'BUTTON' && e.target.className === "add"){
-            fetch(POKEMONS_URL, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "trainer_id": e.target.dataset.trainerId
-                })
-            })
-            .then(resp => resp.json())
-            .then(pokemon => {
-                let ulTag = document.querySelector(`#trainer_${e.target.dataset.trainerId}`)
-                ulTag.innerHTML += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
-            })
-            
-        }
-    })
-})
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>`
+}
+login()
