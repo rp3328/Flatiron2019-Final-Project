889853b8ef7000878332e64197b7db662ba7387f
github username <github email address>
2019-07-23 13:16:15 -0400

    minor debug

--- frontend-finbot/src/dashboard.js
+++ frontend-finbot/src/dashboard.js
@@ -2,7 +2,9 @@ const main = document.querySelector('main')
 
 function showDashboard(user) {
     main.innerHTML = `<h2>Great job logging in ${user.username}!</h2>
-    <button id="logout-button">Logout</button>`
+    <button id="logout-button">Logout</button>
+    <div id=financial-plan> </div>
+    <button id="edit-button">Edit Plan</button>`
 
     //logout functionality
     const logoutButton = document.getElementById(`logout-button`)
@@ -10,4 +12,13 @@ function showDashboard(user) {
       localStorage.removeItem('user_id')
       showLoginScreen()
     })
+    //financial plan view
+    const plan = document.getElementById(`financial-plan`)
+    plan.innerHTML += `
+    Have some kind of plan graphic here...
+    `
+    
+    //edit financial plan
+    const edit = document.getElementById('edit-button')
+    edit.innerHTML += ` have a function that renders an update form`
 }
