f5be9bbffc74ede67f934f9174764da14a2c72bd
github username <github email address>
2019-07-24 08:49:10 -0400

    849am Wed

--- backend-finbot/app/controllers/users_controller.rb
+++ backend-finbot/app/controllers/users_controller.rb
@@ -29,7 +29,15 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    #gets a hash of prices for the user's assets at points in the past
+    def getValue
+        user_id = params[:id]
 
+
+    # returns an array. first element of the array is the interval dates (for the chart x-axis)
+    # each element of the array is an array representing an assetType. It shows the total holdings of that assetType at the interval date
+        byebug
+    end
 
     def update
         user = User.find(params[:id])
